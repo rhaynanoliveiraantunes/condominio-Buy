d802eac09207b67a2f22066514cb17e5eebb61c9
Luiz Gabriel <lgmrttl@gmail.com>
2026-06-18 16:37:08 -0300

    seila

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,12 @@
 import express from "express";
 import userController from "../controllers/userController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import adminMiddleware from "../middlewares/adminMiddleware.js";
+
 
 const router = express.Router();
 
-router.get("/me", authMiddleware, userController.getMe);
-router.put("/me", authMiddleware, userController.updateMe);
+router.post("/purchases", authMiddleware, userController.getMe);
 
-router.get("/", authMiddleware, adminMiddleware, userController.getAllUsers);
-router.get("/:id", authMiddleware, adminMiddleware, userController.getUserById);
-router.put("/:id", authMiddleware, adminMiddleware, userController.updateUser);
-router.patch("/:id/deactivate", authMiddleware, adminMiddleware, userController.deactivateUser);
-router.patch("/:id/activate", authMiddleware, adminMiddleware, userController.activateUser);
+
 
 export default router;
