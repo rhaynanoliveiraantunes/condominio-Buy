96a984fad64524038c857ddc0a25e7f0c1990ff4
Rhaynana Oliveira Antunes <rhaynanoliveiraantunes@gmail.com>
2026-06-18 16:48:54 -0300

    Corrigindo pasta condominio-Buy e adicionando arquivos do backend

---
+++ Condominio-Buy/models/users.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    nome: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    apartamento: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", UserSchema);
