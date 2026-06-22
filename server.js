ea3347b4fac991f4df7d40a8ba283451d3e7b012
Luiz Gabriel <lgmrttl@gmail.com>
2026-06-22 14:09:52 -0300

    seila

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-dotenv.config();
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+import connectDB from './config/db.js'; 
+
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import purchaseRoutes from './routes/purchaseRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
+connectDB();
 
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/purchases', purchaseRoutes);
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "API do CondomínioBuy a funcionar corretamente!" });
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Rota não encontrada." });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor a correr na porta ${PORT}`);
+});
